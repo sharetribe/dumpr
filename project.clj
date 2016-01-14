e7a6f43fb39df2b03e3212b255b5f9bdab58efeb
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2016-01-14 16:00:26 +0200

    master back to 0.1.1-SNAPHOST

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.sharetribe/dumpr "0.1.1-alpha1"
+(defproject org.sharetribe/dumpr "0.1.1-SNAPSHOT"
   :description "Live replicate data from a MySQL database to your own process"
   :url "https://github.com/sharetribe/dumpr"
   :license {:name "Apache License, Version 2.0"
