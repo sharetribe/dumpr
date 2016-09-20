440a1ebea4240baae444299be62d5cd72f7fe3e0
Boyan Tabakov <boyan@sharetribe.com>
2016-09-20 09:46:58 +0300

    Allow test user to connect from any host

--- create_test_db.sql
+++ create_test_db.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS dumpr_test_db_123;
 CREATE DATABASE dumpr_test_db_123;
-GRANT ALL PRIVILEGES on dumpr_test_db_123.* to 'dumpr_test'@'localhost' identified by 'dumpr_test';
-GRANT REPLICATION CLIENT on *.* to 'dumpr_test'@'localhost';
-GRANT REPLICATION SLAVE on *.* to 'dumpr_test'@'localhost';
+GRANT ALL PRIVILEGES on dumpr_test_db_123.* to 'dumpr_test'@'%' identified by 'dumpr_test';
+GRANT REPLICATION CLIENT on *.* to 'dumpr_test'@'%';
+GRANT REPLICATION SLAVE on *.* to 'dumpr_test'@'%';
 Set GLOBAL binlog_format = 'ROW';
