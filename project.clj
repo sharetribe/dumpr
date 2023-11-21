4eb4c03fcb81ba15c3ddf3a6f8e88eaa39f11333
Boyan Tabakov <boyan@sharetribe.com>
2023-11-21 15:57:11 +0200

    Update MySQL client libraries to versions that support MySQL 8.0

--- project.clj
+++ project.clj
@@ -8,10 +8,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [mysql/mysql-connector-java "5.1.39"]
+                 [mysql/mysql-connector-java "8.0.33"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [prismatic/schema "1.0.5"]
-                 [com.github.shyiko/mysql-binlog-connector-java "0.4.1"]
+                 [com.zendesk/mysql-binlog-connector-java "0.28.3"]
                  [manifold "0.1.2"]]
 
   :global-vars {*warn-on-reflection* true}
